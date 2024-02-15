e04ffd192884f7205258f7a016f15d70347dfa93
NirKors <103315566+NirKors@users.noreply.github.com>
2024-02-15 17:28:25 +0200

    Split delete to deletefiles and deletefolder

---
+++ src/actions/deletefiles.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from .action import Action
+
+
+class DeleteFiles(Action):
+    def __init__(self):
+        super().__init__()
+
+
+    def build_ui(self):
+        pass
+
+    def check_for_errors(self):
+        pass
+
+    def get_command_string(self):
+        pass
+
