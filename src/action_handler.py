f0d32ba09d1b92f96549b9c4a15dbb0662f239f6
NirKors <103315566+NirKors@users.noreply.github.com>
2024-02-17 18:01:05 +0200

    Implemented dynamic action class creation using dropdown options

--- src/action_handler.py
+++ src/action_handler.py
@@ -14,22 +14,11 @@ from actions.deletefolders import DeleteFolders
 
 
 def create_action(selected_action, dropdown_options):
-    action_classes = {
-        "Shutdown": Shutdown,
-        "Sleep": Sleep,
-        "Open": Open,
-        "Copy File(s)": CopyFile,
-        "Copy Folder": CopyFolder,
-        "Delete File(s)": DeleteFiles,
-        "Delete Folder": DeleteFolders,
-        "Create": CreateFolder,
-        "Ping Website": PingWebsite,
-        "System Information": SystemInformation,
-        "Cancel Shutdown": CancelShutdown,
-    }
-
+    action_classes = {option: globals().get(option.replace(' ', '')) for option in dropdown_options}
     action_class = action_classes.get(selected_action)
+
     return action_class()
+
 
 def clear_frame(frame):
     counter = 0
