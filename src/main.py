18e7bae6e1172ccc7103327337eeeb2bede18be6
NirKors <nirkors@gmail.com>
2024-02-23 13:30:41 +0200

    Main organization

--- src/main.py
+++ src/main.py
@@ -6,16 +6,19 @@ from ui_engine import UIEngine
 
 
 def main():
-    config_path = "C:\\Users\\nirko\\PycharmProjects\\ScriptSculptor\\config"
-    config_path = "C:\\Users\\Nir\\PycharmProjects\\ScriptSculptor\\config"
 
     root = tk.Tk()
+
+    # Styling
+    config_path = "C:\\Users\\Nir\\PycharmProjects\\ScriptSculptor\\config"
+    config_path = "C:\\Users\\nirko\\PycharmProjects\\ScriptSculptor\\config"
     config = ConfigParser()
     config.read(f'{config_path}\\settings.ini')
     colors = config["colors"]
-    style = ttk.Style()
-    style.theme_use("default")
 
+    style = ttk.Style()
+
+    style.theme_use("default")
     style.configure("buttons_frame.TFrame", padding=6, background=colors["buttons_frame"])
     style.configure("background.TFrame", padding=6, background=colors["background"])
     style.configure("script_frame.TFrame", padding=6, background=colors["script_frame"])
@@ -26,8 +29,11 @@ def main():
               background=[("active", "white"), ("!active", "black")],
               foreground=[("active", "black"), ("!active", "white")])
 
+
     app = UIEngine(root, config_path)
     root.mainloop()
+
+
 
 
 if __name__ == "__main__":
