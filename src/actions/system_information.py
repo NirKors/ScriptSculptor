853b5932dc02bdf0a39f192c41f925d2e203af45
NirKors <nirkors@gmail.com>
2024-02-23 13:30:59 +0200

    Sys info initial commit

--- src/actions/system_information.py
+++ src/actions/system_information.py
@@ -4,9 +4,24 @@ from .action import Action
 
 class SystemInformation(Action):
     def __init__(self):
-        pass
+        super().__init__()
+
 
     def build_ui(self):
+
+
+
+        tooltip = """
+Displays detailed configuration information about a computer and its operating system, including operating
+system configuration, security information, product ID, and hardware properties (such as RAM, disk space,
+and network cards).
+"""
+        self.explanatory_tooltip(tooltip)
+
+    def check_for_warnings(self):
+        pass
+
+    def check_for_errors(self):
         pass
 
     def get_command_string(self):
