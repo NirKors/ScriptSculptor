e2ef54450833ff2e9c7bc2ef0ccc38368927e3a4
NirKors <nirkors@gmail.com>
2024-05-07 13:08:16 +0300

    Added comments to action_handler.py methods.

--- src/action_handler.py
+++ src/action_handler.py
@@ -12,6 +12,19 @@ from actions.openfolder import OpenFolder
 
 
 def create_action(selected_action, dropdown_options):
+    """
+    Create an instance of the selected action class.
+
+    This function dynamically selects and creates an instance of the action class
+    corresponding to the selected action from the dropdown menu.
+
+    Args:
+        selected_action (str): The selected action from the dropdown menu.
+        dropdown_options (list): List of available action options.
+
+    Returns:
+        Instance of the selected action class.
+    """
     action_classes = {option: globals().get(option.replace(' ', '')) for option in dropdown_options}
     action_class = action_classes.get(selected_action)
 
@@ -19,6 +32,18 @@ def create_action(selected_action, dropdown_options):
 
 
 def clear_frame(frame):
+    """
+    Clear the contents of a Tkinter frame.
+
+    This function removes all children of the specified Tkinter frame, except for the first three children.
+    The first three children are the UI elements that exist in every action frame.
+
+    Args:
+        frame: Tkinter frame to clear.
+
+    Returns:
+        None
+    """
     counter = 0
     for child in frame.winfo_children():
         if counter >= 3:
