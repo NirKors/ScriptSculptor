89d899b400764a91a762179449bb51bba9be716a
NirKors <103315566+NirKors@users.noreply.github.com>
2024-05-13 13:45:08 +0300

    Added new icon

--- src/main.py
+++ src/main.py
@@ -5,9 +5,13 @@ from ui_engine import UIEngine
 
 def main():
     root = tk.Tk()
+
     script_path = os.path.dirname(__file__)
     script_dir, _ = os.path.split(script_path)
     config_dir = os.path.join(script_dir, "config")
+
+    image = tk.PhotoImage(file=f"{script_path}\\assets\\icon.png")
+    root.iconphoto(False, image)
 
     UIEngine(root, config_dir)
     root.mainloop()
